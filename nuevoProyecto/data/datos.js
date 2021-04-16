c4b0362153cc18c4b807da44e448ac7696f977ff
Alexiaalurralde <aalurralde@udesa.edu.ar>
2021-04-16 20:43:54 -0300

    ordeno y agrego formulario productAdd

--- nuevoProyecto/data/datos.js
+++ nuevoProyecto/data/datos.js
@@ -1,49 +1,35 @@
 const products = {
-lista: [
+    lista: [
+        {
+            nombre:"Nike Jordan High OG",
+            descripcion: "Descripcion de la zapatilla",
+            imagen: "/images/products/IMG_6466.jpg",
+            comentarios:"9",
+            id: 1,
+        },
+        {
+            nombre:"Nike Jordan High GG",
+            descripcion: "Descripcion de la zapatilla",
+            imagen: "/images/products/IMG_6494.jpg",
+            comentarios:"3",
+            id:2
+        },
+        {
+            nombre:"Nike Jordan Retro OG",
+            descripcion: "Descripcion de la zapatilla",
+            imagen: "/images/products/IMG_6469.jpg",
+            comentarios:"4",
+            id:3
+        },
+        {
+            nombre:"Nike Jordan Mid Crimson",
+            descripcion: "Descripcion de la zapatilla",
+            imagen: "/images/products/IMG_6667.jpg",
+            comentarios:"27",
+            id:4
+        },
 
-    {nombre:"Comme des garcon",
-    descripcion: " hola soy la zapatilla",
-    imagen: "/images/products/IMG_6465.jpg",
-    comentarios:"9",
-    id: 1,
-    },
-    {nombre:" Yeezy boost 350 V2",
-    descripcion: " hola soy la zapatilla",
-    imagen: "/images/products/IMG_6466.jpg",
-    comentarios:"3",
-    id:2
-    },
-    {nombre:" Comme des garcon",
-    descripcion: " hola soy la zapatilla",
-    imagen: "/images/products/IMG_6487.jpg",
-    comentarios:"4",
-    id:3
-    },
-    {nombre:" Yeezy boost 350 V2",
-    descripcion: " hola soy la zapatilla",
-    imagen: "/images/products/IMG_6469.jpg",
-    comentarios:"27",
-    id:4
-    },
-
-],
-
-users: [
-    { 
-    nombre: "Franco Murina",
-    upDatedAt: "2020-03-03",
-    edad: 18
-    }
-],
-
-comentarios: [
-    {
-    nombreUsuario: "Franco Murina",
-    imagenPerfil: "",
-    texto: ""
-    }
-]
-
+    ]
 }
 
 module.exports = products
