13b96bc665429e29027797ce1f1645e1cf249573
Alexiaalurralde <aalurralde@udesa.edu.ar>
2021-04-16 19:49:38 -0300

    cambios pocos

--- nuevoProyecto/data/datos.js
+++ nuevoProyecto/data/datos.js
@@ -1,39 +1,46 @@
 const products = {
 lista: [
 
-    {nombre:" Comme des garcon",
+    {nombre:"Comme des garcon",
     descripcion: " hola soy la zapatilla",
-    imagen: "/images/converse.webp",
+    imagen: "/images/products/IMG_6465.jpg",
     comentarios:"9",
     id: 1,
     },
     {nombre:" Yeezy boost 350 V2",
     descripcion: " hola soy la zapatilla",
-    imagen: "/images/yeezy.jpeg",
+    imagen: "/images/products/IMG_6466.jpg",
     comentarios:"3",
     id:2
     },
     {nombre:" Comme des garcon",
     descripcion: " hola soy la zapatilla",
-    imagen: "/images/converse.webp",
+    imagen: "/images/products/IMG_6487.jpg",
     comentarios:"4",
     id:3
     },
     {nombre:" Yeezy boost 350 V2",
     descripcion: " hola soy la zapatilla",
-    imagen: "/images/yeezy.jpeg",
+    imagen: "/images/products/IMG_6469.jpg",
     comentarios:"27",
     id:4
     },
 
 ],
 
-usuarios: [
+users: [
     { 
     nombre: "Franco Murina",
     upDatedAt: "2020-03-03",
-    edad: 18,
+    edad: 18
+    }
+],
 
+comentarios: [
+    {
+    nombreUsuario: "Franco Murina",
+    imagenPerfil: "",
+    texto: ""
     }
 ]
 
