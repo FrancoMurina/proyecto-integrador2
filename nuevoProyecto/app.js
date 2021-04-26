3eeb4455ec0bf1903adca8032901e0a22095f19a
noelrussomendez <mrussomendez@udesa.edu.ar>
2021-04-25 23:44:42 -0300

    app.js

--- nuevoProyecto/app.js
+++ nuevoProyecto/app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 const productRouter = require('./routes/product');
 const loginRegisterRouter = require('./routes/loginRegister');
 const searchResultsRouter = require('./routes/searchResults');
